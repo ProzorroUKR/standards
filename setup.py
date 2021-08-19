4a300bbb587cf8ab0e84806509754e55e919a38a
Aleksey Stryukov <aleksey.stryukov@gmail.com>
2021-08-19 17:27:54 +0300

    Release 1.0.18

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="standards",
-    version="1.0.17",
+    version="1.0.18",
     description="Prozorro standards",
     python_requires=">=2.7",
     package_dir={'standards': '.'},
