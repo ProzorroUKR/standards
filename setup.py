c85d5211b9f7cf88a58acfd6564ab6f3174629b0
Aleksey Stryukov <aleksey.stryukov@gmail.com>
2022-04-21 15:42:32 +0300

    Mask codes update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="standards",
-    version="1.0.33",
+    version="1.0.34",
     description="Prozorro standards",
     python_requires=">=2.7",
     package_dir={'standards': '.'},
