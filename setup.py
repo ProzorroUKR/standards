96cd74ad6f8f3a829827a69ae8e47447603a2f58
smithumble <33865176+smithumble@users.noreply.github.com>
2023-05-02 00:43:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="standards",
-    version="1.0.49",
+    version="1.0.54",
     description="Prozorro standards",
     python_requires=">=2.7",
     package_dir={'standards': '.'},
