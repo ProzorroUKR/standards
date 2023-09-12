0c0803a022c63bc16e7282bed0338db3a1f44b5c
Serhij Andreev <serhij.andreev@raccoongang.com>
2023-09-12 17:37:47 +0300

    add exist method and templated/contracts dir

--- __init__.py
+++ __init__.py
@@ -1,9 +1,16 @@
 import os.path
 from json import loads
 
+
 def load(path):
     this_dir, this_filename = os.path.split(__file__)
     file_path = os.path.join(this_dir, path)
     with open(file_path) as f:
         data = f.read()
     return loads(data)
+
+
+def exist(path):
+    this_dir, this_filename = os.path.split(__file__)
+    file_path = os.path.join(this_dir, path)
+    return os.path.exists(file_path)
