22a3e6456cafb810fe7a05c828680352ff161b33
Serhij Andreev <serhij.andreev@raccoongang.com>
2023-09-21 12:58:22 +0300

    update get_dir_files

--- __init__.py
+++ __init__.py
@@ -18,4 +18,6 @@ def exist(path):
 
 
 def get_dir_files(path):
-    return next(walk(path), (None, None, []))[2]
+    this_dir, this_filename = os.path.split(__file__)
+    dir_path = os.path.join(this_dir, path)
+    return next(walk(dir_path), (None, None, []))[2]
