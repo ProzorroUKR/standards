8303953deb897ea654063c6df1c66ade207f3798
Serhij Andreev <serhij.andreev@raccoongang.com>
2023-09-27 01:42:32 +0300

    remove useless methods

--- __init__.py
+++ __init__.py
@@ -9,15 +9,3 @@ def load(path):
     with open(file_path) as f:
         data = f.read()
     return loads(data)
-
-
-def exist(path):
-    this_dir, this_filename = os.path.split(__file__)
-    file_path = os.path.join(this_dir, path)
-    return os.path.exists(file_path)
-
-
-def get_dir_files(path):
-    this_dir, this_filename = os.path.split(__file__)
-    dir_path = os.path.join(this_dir, path)
-    return next(walk(dir_path), (None, None, []))[2]
