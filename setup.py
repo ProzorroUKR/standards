8d27bb4706358fc2ac75fb5f290a72a16af1543e
Olha Zamirets <ozamirets@gmail.com>
2023-10-18 12:50:03 +0200

    Release 1.0.70

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="standards",
-    version="1.0.69",
+    version="1.0.70",
     description="Prozorro standards",
     python_requires=">=2.7",
     package_dir={'standards': '.'},
