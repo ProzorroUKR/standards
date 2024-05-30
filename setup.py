0b0f4a1cdc6b74dd3413b8b3fe64849186ae6f05
smithumble <33865176+smithumble@users.noreply.github.com>
2024-05-30 13:18:08 +0300

    Release 1.0.91

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="standards",
-    version="1.0.90",
+    version="1.0.91",
     description="Prozorro standards",
     python_requires=">=2.7",
     package_dir={'standards': '.'},
