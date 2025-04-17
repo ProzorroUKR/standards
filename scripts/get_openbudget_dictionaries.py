0d763490a5c688bad07a519c28a5ddee886bfecb
Olha Zamirets <ozamirets@gmail.com>
2025-04-17 11:41:41 +0300

    Add field `years` for KPK classifier

--- scripts/get_openbudget_dictionaries.py
+++ scripts/get_openbudget_dictionaries.py
@@ -1,4 +1,6 @@
 import json
+from copy import deepcopy
+
 import requests
 
 from datetime import datetime
@@ -14,16 +16,20 @@ def get_openbudget_dictionary():
             data = response.json()
             if dict_name == "KPK":
                 export_kpk(dict_name, data)
-            write_json_dict(dict_name, data)
+            else:
+                write_json_dict(dict_name, data)
         else:
             print(f"Response from resource {resource_url}: {response.status_code} - {response.text}")
 
 
-def write_json_dict(dict_name, dict_data):
-    result = {
-        item["code"]: {k: v for k, v in item.items() if k != "code"}
-        for item in dict_data
-    }
+def write_json_dict(dict_name, dict_data, split_by_code=True):
+    if split_by_code:
+        result = {
+            item["code"]: {k: v for k, v in item.items() if k != "code"}
+            for item in dict_data
+        }
+    else:
+        result = dict_data
     json_object = json.dumps(result, indent=2, ensure_ascii=False)
     with open(f'./classifiers/{dict_name.lower()}.json', 'w') as outfile:
         outfile.write(json_object)
@@ -32,9 +38,8 @@ def write_json_dict(dict_name, dict_data):
 
 def export_kpk(dict_name, dict_data):
     kpk_years = {}
+    kpk_general = []
     for kpk_row in dict_data:
-        if not kpk_row.get("datefrom"):
-            print(kpk_row["code"])
         year_from = datetime.strptime(kpk_row.get("datefrom"), "%Y-%m-%d").year
         # By default, we take current year, if there wasn't `dateto` in dictionary that means that code is still using
         year_to = datetime.now().year
@@ -43,12 +48,18 @@ def export_kpk(dict_name, dict_data):
         if date_to := kpk_row.get("dateto"):
             year_to = datetime.strptime(date_to, "%Y-%m-%d").year
         dict_year = year_from
+        kpk_code_years = []  # array for collecting years for particular code
         while dict_year <= year_to:
             kpk_years.setdefault(dict_year, []).append(kpk_row)
+            kpk_code_years.append(dict_year)
             dict_year += 1
+        kpk_code_data = deepcopy(kpk_row)
+        kpk_code_data["years"] = kpk_code_years
+        kpk_general.append(kpk_code_data)
 
     for year in kpk_years.keys():
         write_json_dict(f"{dict_name}_{year}", kpk_years[year])
+    write_json_dict(dict_name, kpk_general, split_by_code=False)
 
 
 if __name__ == "__main__":
