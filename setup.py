610fab8adbefa915405229d3247fd12798ae549b
Olha Zamirets <ozamirets@gmail.com>
2025-10-02 17:17:17 +0300

    Update KPK and TKPKMB codes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="standards",
-    version="1.0.184.3",
+    version="1.0.184.4",
     description="Prozorro standards",
     python_requires=">=2.7",
     package_dir={'standards': '.'},
