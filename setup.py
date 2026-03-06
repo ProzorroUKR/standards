65a1eb7d1e931bd5785ce0b04829c219c638fcde
mhoang-rg <hoang.viet.ha@raccoongang.com>
2026-03-06 10:29:08 +0200

    Release 1.0.232

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="standards",
-    version="1.0.231",
+    version="1.0.232",
     description="Prozorro standards",
     python_requires=">=2.7",
     package_dir={'standards': '.'},
