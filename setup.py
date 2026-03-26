ac4fc740dffc327e5e76de9caeaa534c4826d754
smithumble <33865176+smithumble@users.noreply.github.com>
2026-03-26 21:06:40 +0200

    Release 1.0.238

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="standards",
-    version="1.0.237",
+    version="1.0.238",
     description="Prozorro standards",
     python_requires=">=2.7",
     package_dir={'standards': '.'},
