bc5267dbbb6ff1ad299395dfe749abb469f7f447
Oleksandr Hrekov <alexander.greckov@raccoongang.com>
2026-07-27 13:53:20 +0300

    Bump version from 1.0.264 to 1.0.265

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="standards",
-    version="1.0.264",
+    version="1.0.265",
     description="Prozorro standards",
     python_requires=">=2.7",
     package_dir={'standards': '.'},
