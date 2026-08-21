c32ccbf2bc9af3107e50ffaeff8a4fc286065555
Oleksandr Hrekov <252195716+oleksandr-hrekov@users.noreply.github.com>
2026-08-21 12:38:26 +0300

    Release 1.0.274

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="standards",
-    version="1.0.273",
+    version="1.0.274",
     description="Prozorro standards",
     python_requires=">=2.7",
     package_dir={'standards': '.'},
