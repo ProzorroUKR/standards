81e7ef26fbae09512497fe98bf0dd614a2ec86d1
Mykhailo Myroshnichenko <mykhailo.myroshnichenko@users.noreply.github.com>
2026-09-03 12:53:38 +0300

    Release 1.0.279

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="standards",
-    version="1.0.278",
+    version="1.0.279",
     description="Prozorro standards",
     python_requires=">=2.7",
     package_dir={'standards': '.'},
